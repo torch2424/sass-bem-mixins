0b5d7df18d3c5ba62bd5ba177598c58228f9977e
Aaron Turner <aaron@aaronthedev.com>
2017-08-19 01:06:40 -0700

    Added an example gh-pages link

--- gh-pages.js
+++ gh-pages.js
@@ -3,7 +3,7 @@ const cheerio = require('cheerio');
 // Load our current index.html
 const $ = cheerio.load(fs.readFileSync('./index.html'));
 // Replace the inline amp-custom css
-$('style[amp-custom]').textContent = fs.readFileSync('./index.css');
+$('style[amp-custom]').text(`\n${fs.readFileSync('./gh-pages.css')}`);
 
 // Write back to our index.html
 fs.writeFileSync('./index.html', $.html());
